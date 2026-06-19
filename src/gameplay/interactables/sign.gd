15afbda9ee946a3ddca74ca4839c146ef94522dc
dr-rodriguez <strakul@gmail.com>
2026-06-19 09:21:57 -0400

    initial dialogue balloon

---
+++ src/gameplay/interactables/sign.gd
@@ -0,0 +1,12 @@
+extends StaticBody2D
+
+@onready var interactable_component: Interactable = $InteractableComponent
+
+func _ready():
+	interactable_component.interact = Callable(self, "_on_interact")
+	
+func _on_interact():
+	# Dialogue logic
+	var balloon_scene = load("res://src/resources/dialogue/balloon/balloon.tscn")
+	var resource = load("res://src/resources/dialogue/misc.dialogue")
+	DialogueManager.show_dialogue_balloon_scene(balloon_scene, resource, "sign")
