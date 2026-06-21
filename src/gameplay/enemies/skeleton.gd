9d900cddd7c75a4bf9f856ab779f8096dde51999
dr-rodriguez <strakul@gmail.com>
2026-06-21 15:31:48 -0400

    updating skeleton logic

--- src/gameplay/enemies/skeleton.gd
+++ src/gameplay/enemies/skeleton.gd
@@ -1,9 +1,18 @@
 extends CharacterBody2D
 
 @export var data: EnemyData
+var direction: Vector2 = Vector2.ZERO
+var last_direction: Vector2 = Vector2.ZERO
 
 @onready var anim: AnimatedSprite2D = $AnimatedSprite2D
+@onready var state_machine: StateMachine = $States
+
 
 func _ready() -> void:
 	anim.sprite_frames = data.frames
 	anim.play(data.default_anim)
+
+
+func _physics_process(_delta: float) -> void:
+	# TODO: Add some random movement
+	pass
